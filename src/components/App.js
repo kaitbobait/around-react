b5f092c3126f150382fa7bb8177ff256f1d4f225
kaithill <k.m.hiller@gmail.com>
2021-02-19 17:01:39 -0500

    create and import Main.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 
 import '../index.css';
 import Header from './Header.js';
+import Main from './Main.js';
 import Footer from './Footer.js';
 
 
@@ -9,29 +10,7 @@ function App() {
     <body class="page">
       <div className="page__content">
         < Header />
-        
-        <main className="main">
-          <section className="profile">
-            <div className="profile__img"></div>
-            <button className ="profile__img-edit" aria-label="edit-picture" type="button"></button>
-            <div className="profile__description">
-                <div className="profile__description-top">
-                  <h1 className="profile__name">Jacques Cousteau</h1>
-                  <button className="profile__edit" aria-label="edit" type="button">
-                  </button>  
-                </div>
-              <p className="profile__title">Explorer</p>
-            </div>
-            <button className="profile__add" aria-label="add" type="button"></button>
-          </section>
-          {/* <!-- popup section after created below --> */}
-          <section className="places">
-            <ul className="places__list">
-              
-            </ul>
-          </section>
-        </main>
-
+        < Main />
         < Footer />
         <div className="popup popup_edit-profile">
           <div className="popup__container">
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,28 @@
 
 function Main() {
   return(
-    
+    <main className="main">
+          <section className="profile">
+            <div className="profile__img"></div>
+            <button className ="profile__img-edit" aria-label="edit-picture" type="button"></button>
+            <div className="profile__description">
+                <div className="profile__description-top">
+                  <h1 className="profile__name">Jacques Cousteau</h1>
+                  <button className="profile__edit" aria-label="edit" type="button">
+                  </button>  
+                </div>
+              <p className="profile__title">Explorer</p>
+            </div>
+            <button className="profile__add" aria-label="add" type="button"></button>
+          </section>
+          {/* <!-- popup section after created below --> */}
+          <section className="places">
+            <ul className="places__list">
+              
+            </ul>
+          </section>
+        </main>
   )
 }
+
+export default Main;

f8b1b7d52a18a92ef0ec5ddc48519cda3826b876
kaithill <k.m.hiller@gmail.com>
2021-02-22 21:29:21 -0500

    add onClick to <img> in Main.js

--- src/components/App.js
+++ src/components/App.js
@@ -37,9 +37,9 @@ function App() {
 
   const [selectedCard, setSelectedCard] = React.useState(false);
   
-  function handleCardClick(card) {
+  function handleCardClick() {
     //should add which image address in img tag to popup
-    setSelectedCard(card);
+    setSelectedCard();
     
   }
   

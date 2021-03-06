2a28044f4c5bbb661be2e8c67a864f6f6f04994d
kaithill <k.m.hiller@gmail.com>
2021-03-06 18:16:36 -0500

    add onChange values to edit profile popup

--- src/components/App.js
+++ src/components/App.js
@@ -80,7 +80,7 @@ function App() {
             onCardClick={handleCardClick}
           />
           < Footer />
-          {/* <EditProfilePopup isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} /> */}
+          <EditProfilePopup isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} />
 
           <PopupWithForm
             onClose={closeAllPopups}
--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,29 +1,32 @@
 
-// import React from 'react';
-// import PopupWithForm from './PopupWithForm.js';
+import React from 'react';
+import PopupWithForm from './PopupWithForm.js';
 
-// export function EditProfilePopup(props) {
+function EditProfilePopup(props) {
 
-  // const [name, setName] = React.useState();
-//   const [description, setDescription] = React.useState();
+  const [name, setName] = React.useState("name");
+  const [description, setDescription] = React.useState("description");
 
-      // function handleChange(evt) {
-      //   setName(evt.target.value);
-      // }
+  function handleNameChange(evt) {
+    setName(evt.target.value);
+  }
 
-//   return(
-//       <>
-//           <PopupWithForm
-//             onClose={closeAllPopups}
-//           />
+  function handleDescriptionChange(evt) {
+    setDescription(evt.target.value);
+  }
 
-//           <PopupWithForm name="profile-edit" title="Edit Profile" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
-//             <input className="popup__input popup__input_text_name" id="input_text_name" type="text" name="profile-edit" placeholder="name" minLength="2" maxLength="40" required />
-//             <span className="popup__input-error" id="input_text_name-error"></span>
-//             <input className="popup__input popup__input_text_title" id="input_text_title" type="text" name="profile-edit" placeholder="Occupation" minLength="2" maxLength="200" required />
-//             <span className="popup__input-error" id="input_text_title-error"></span>
-//           </PopupWithForm>
-//       </>
+  return (
+   
+      
+      <PopupWithForm name="profile-edit" title="Edit Profile" isOpen={props.isOpen} onClose={props.onClose}>
+        <input className="popup__input popup__input_text_name" id="input_text_name" value={name} onChange={handleNameChange} type="text" name="profile-edit" placeholder="name" minLength="2" maxLength="40" required />
+        <span className="popup__input-error" id="input_text_name-error"></span>
+        <input className="popup__input popup__input_text_title" id="input_text_title" value={description} onChange={handleDescriptionChange} type="text" name="profile-edit" placeholder="Occupation" minLength="2" maxLength="200" required />
+        <span className="popup__input-error" id="input_text_title-error"></span>
+      </PopupWithForm>
+    
 
-//   )
-// }
+  )
+}
+
+export default EditProfilePopup;
